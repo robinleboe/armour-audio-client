3aeb6d39eaaa506d612665ab8b860a62ff88e3d7
Robin Leboe <robin.leboe@gmail.com>
2019-12-02 15:35:32 -0800

    update store and add Provider to app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import jwtDecode from 'jwt-decode';
 import './App.css';
 
+// redux
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 // MUI
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
@@ -34,20 +38,30 @@ if (token) {
 
 function App() {
   return (
-    <div className="App">
     <MuiThemeProvider theme={theme}>
-      <Router>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <AuthRoute exact path="/" component={home} />
-            <AuthRoute exact path="/login" component={login} authenticated={authenticated}/>
-            <AuthRoute exact path="/signup" component={signup} authenticated={authenticated}/>
-          </Switch>
-        </div>
-      </Router>
-      </MuiThemeProvider>
-    </div>
+      <Provider store={store}>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <AuthRoute exact path="/" component={home} />
+              <AuthRoute
+                exact
+                path="/login"
+                component={login}
+                authenticated={authenticated}
+              />
+              <AuthRoute
+                exact
+                path="/signup"
+                component={signup}
+                authenticated={authenticated}
+              />
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
+    </MuiThemeProvider>
   );
 }
 
