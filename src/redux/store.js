ec3303d4650012fef74bf33739914e9a990b4604
Robin Leboe <robin.leboe@gmail.com>
2019-12-02 15:12:20 -0800

    store.js intitialized

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,27 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { thunk } from 'redux-thunk';
 
-const initialState = {
+import userReducer from './reducers/userReducer';
+import dataReducer from './reducers/dataReducer';
+import uiReducer from './reducers/uiReducer';
 
-};
+const initialState = {};
 
 const middleware = [thunk];
 
+const reducers = combineReducers({
+  user: userReducer,
+  data: dataReducer,
+  UI: uiReducer
+});
+
+const store = createStore(
+  reducers,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
