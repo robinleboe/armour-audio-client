c64f6ec1b072b23b01453b77d231edad83819e30
Robin Leboe <robin.leboe@gmail.com>
2019-12-05 17:15:42 -0800

    update types for data actions

--- src/redux/types.js
+++ src/redux/types.js
@@ -8,3 +8,7 @@ export const SET_ERRORS = 'SET_ERRORS';
 export const LOADING_UI = 'LOADING_UI';
 export const CLEAR_ERRORS = 'CLEAR_ERRORS';
 // data reducer types
+export const SET_NOTES = 'SET_NOTES';
+export const LOADING_DATA = 'LOADING_DATA';
+export const LIKE_NOTE = 'LIKE_NOTE';
+export const UNLIKE_NOTE = 'UNLIKE_NOTE';
